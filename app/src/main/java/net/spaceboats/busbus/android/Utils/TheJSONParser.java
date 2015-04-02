6efe8ecb0010e1b20e955200d4cbe57d733b4b7c
Zane Ralston <zralston88@gmail.com>
2015-04-02 00:55:23 -0500

    Updated TheJSONParser to use the new JSONKeys class

--- app/src/main/java/net/spaceboats/busbus/android/Utils/TheJSONParser.java
+++ app/src/main/java/net/spaceboats/busbus/android/Utils/TheJSONParser.java
@@ -17,31 +17,11 @@ import java.util.List;
  */
 public class TheJSONParser {
 
-    public static final String ROUTES_JSON_ID = "routes";
-    public static final String ROUTE_JSON_ID = "route";
-    public static final String ROUTE_DESCRIPTION_JSON_ID = "description";
-    public static final String ROUTE_JSON_SHORT_NAME = "short_name";
-    public static final String ROUTE_JSON_NAME = "name";
-    public static final String ROUTE_JSON_NUMBER = "id";
-    public static final String ROUTE_JSON_COLOR = "color";
-
-    public static final String STOP_JSON_MYID = "id";
-    public static final String STOPS_JSON_ID = "stops";
-    public static final String STOP_JSON_ID = "stop";
-    public static final String STOP_JSON_NAME = "name";
-    public static final String STOP_JSON_DESCRIPTION = "description";
-    public static final String STOP_JSON_LATITUDE = "latitude";
-    public static final String STOP_JSON_LONGITUDE = "longitude";
-    public static final String ARRIVALS_JSON_ID = "arrivals";
-    public static final String ARRIVAL_JSON_HEADSIGN = "headsign";
-    public static final String ARRIVAL_JSON_TIME = "time";
-
-
     public static List<Entity> getRouteList(String data)
         throws JSONException {
 
         JSONObject dataObj = new JSONObject(data);
-        JSONArray routesArray = dataObj.getJSONArray(ROUTES_JSON_ID);
+        JSONArray routesArray = dataObj.getJSONArray(JSONKeys.ENTITY_ROUTES);
         List<Entity> entities = new ArrayList<>();
         for (int i = 0; i < routesArray.length(); i++) {
             JSONObject routeObj = routesArray.getJSONObject(i);
@@ -53,14 +33,14 @@ public class TheJSONParser {
 
     private static Route getRoute(JSONObject routeObj)
         throws JSONException {
-        if (routeObj.isNull(ROUTE_JSON_SHORT_NAME) || routeObj.isNull(ROUTE_JSON_NAME))
+        if (routeObj.isNull(JSONKeys.ROUTE_SHORT_NAME) || routeObj.isNull(JSONKeys.ROUTE_NAME))
             return null;
-        String routeName = routeObj.getString(ROUTE_JSON_NAME);
-        String routeNumber = routeObj.getString(ROUTE_JSON_SHORT_NAME);
+        String routeName = routeObj.getString(JSONKeys.ROUTE_NAME);
+        String routeNumber = routeObj.getString(JSONKeys.ROUTE_SHORT_NAME);
         String routeColor = "#424242";
-        if (!routeObj.isNull(ROUTE_JSON_COLOR))
-            routeColor = "#" + routeObj.getString(ROUTE_JSON_COLOR);
-        String routeId = routeObj.getString(ROUTE_JSON_NUMBER);
+        if (!routeObj.isNull(JSONKeys.ROUTE_COLOR))
+            routeColor = "#" + routeObj.getString(JSONKeys.ROUTE_COLOR);
+        String routeId = routeObj.getString(JSONKeys.ROUTE_ID);
         return new Route(routeNumber, routeName, routeColor, routeId);
     }
 
@@ -68,7 +48,7 @@ public class TheJSONParser {
         throws JSONException {
 
         JSONObject dataObj = new JSONObject(data);
-        JSONArray stopsArray = dataObj.getJSONArray(STOPS_JSON_ID);
+        JSONArray stopsArray = dataObj.getJSONArray(JSONKeys.ENTITY_STOPS);
         List<Entity> entities = new ArrayList<>();
         for(int i = 0; i < stopsArray.length(); i++) {
             JSONObject stopObj = stopsArray.getJSONObject(i);
@@ -82,13 +62,13 @@ public class TheJSONParser {
         throws JSONException {
         String stopName = "";
         String stopDescription = "";
-        if (!stopObj.isNull(STOP_JSON_NAME))
-            stopName = stopObj.getString(STOP_JSON_NAME);
-        if (!stopObj.isNull(STOP_JSON_DESCRIPTION))
-            stopDescription = stopObj.getString(STOP_JSON_DESCRIPTION);
-        double latitude = stopObj.getDouble(STOP_JSON_LATITUDE);
-        double longitude = stopObj.getDouble(STOP_JSON_LONGITUDE);
-        String stopId = stopObj.getString(STOP_JSON_MYID);
+        if (!stopObj.isNull(JSONKeys.STOP_NAME))
+            stopName = stopObj.getString(JSONKeys.STOP_NAME);
+        if (!stopObj.isNull(JSONKeys.STOP_DESCRIPTION))
+            stopDescription = stopObj.getString(JSONKeys.STOP_DESCRIPTION);
+        double latitude = stopObj.getDouble(JSONKeys.STOP_LATITUDE);
+        double longitude = stopObj.getDouble(JSONKeys.STOP_LONGITUDE);
+        String stopId = stopObj.getString(JSONKeys.STOP_ID);
         return new Stop(stopName, latitude, longitude, stopDescription, stopId);
     }
 
@@ -96,7 +76,7 @@ public class TheJSONParser {
         throws JSONException {
 
         JSONObject dataObj = new JSONObject(data);
-        JSONArray arrivalsArray = dataObj.getJSONArray(ARRIVALS_JSON_ID);
+        JSONArray arrivalsArray = dataObj.getJSONArray(JSONKeys.ENTITY_ARRIVALS);
         List<Entity> entities = new ArrayList<>();
         for(int i = 0; i < arrivalsArray.length(); i++) {
             JSONObject arrivalObj = arrivalsArray.getJSONObject(i);
@@ -109,15 +89,15 @@ public class TheJSONParser {
     private static Arrival getArrival(JSONObject arrivalObj)
         throws JSONException {
 
-        JSONObject stopObj = arrivalObj.getJSONObject(STOP_JSON_ID);
-        JSONObject routeObj = arrivalObj.getJSONObject(ROUTE_JSON_ID);
+        JSONObject stopObj = arrivalObj.getJSONObject(JSONKeys.ARRIVAL_STOP);
+        JSONObject routeObj = arrivalObj.getJSONObject(JSONKeys.ARRIVAL_ROUTE);
         Route route = getRoute(routeObj);
         if(route == null)
             return null;
-        int time = arrivalObj.getInt(ARRIVAL_JSON_TIME);
+        int time = arrivalObj.getInt(JSONKeys.ARRIVAL_TIME);
         String headSign = "";
-        if(!arrivalObj.isNull(ARRIVAL_JSON_HEADSIGN))
-            headSign = arrivalObj.getString(ARRIVAL_JSON_HEADSIGN);
+        if(!arrivalObj.isNull(JSONKeys.ARRIVAL_HEADSIGN))
+            headSign = arrivalObj.getString(JSONKeys.ARRIVAL_HEADSIGN);
         return new Arrival(time, headSign, getStop(stopObj), route);
     }
 }
