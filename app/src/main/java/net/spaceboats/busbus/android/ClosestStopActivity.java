6add86dde8df8f8922351ebdba2079743e7ae43b
Alex Gustafson <alexdgus@gmail.com>
2015-04-08 16:51:41 -0500

    Added intent to launch the closest stops
    When the gps location changes from the main activity, the closest
    stops activity is now launched.

--- app/src/main/java/net/spaceboats/busbus/android/ClosestStopActivity.java
+++ app/src/main/java/net/spaceboats/busbus/android/ClosestStopActivity.java
@@ -2,16 +2,26 @@ package net.spaceboats.busbus.android;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import net.spaceboats.busbus.android.DbHelper.EntityDbDelegator;
+
 
 public class ClosestStopActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closest_stop);
+
+        EntityDbDelegator.initDbDelegator(getApplicationContext());
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
 
